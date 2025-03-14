d208e8f58522764aa0bd543fc6229a41cd20dd22
bimohenokh <bimo.barata2@gmail.com>
2025-03-14 21:11:50 +0700

    nambah model role, permission, rolepermission

--- taschoolassistant/users/models.py
+++ taschoolassistant/users/models.py
@@ -1,8 +1,20 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import EmailField
+from django.db.models import EmailField, Model, CharField, ForeignKey, CASCADE
 
 
 # Create your models here.
-
 class User(AbstractUser):
     email = EmailField(unique=True)
+
+
+class Role(Model):
+    name = CharField(max_length=50, unique=True)
+
+
+class Permission(Model):
+    name = CharField(max_length=100, unique=True)
+
+
+class RolePermission(Model):
+    role = ForeignKey(Role, on_delete=CASCADE)
+    permission = ForeignKey(Permission, on_delete=CASCADE)
