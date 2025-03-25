d666a87350ad3f295278b7d31f06da7bb207a1c7
Daffa Maulana Haekal <maulanadaffa342l@gmail.com>
2025-03-25 16:54:29 +0700

    [GREEN] handle image post and put

--- taschoolassistant/courses/urls.py
+++ taschoolassistant/courses/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import path
 from .views import CourseView, CourseViewById, CourseSessionView, CourseSessionViewById
 
@@ -7,3 +8,5 @@ urlpatterns = [
     path("course/<int:course_id>/", CourseSessionView.as_view(), name="course-session"),
     path("course/<int:course_id>/session/<int:session_id>/", CourseSessionViewById.as_view(), name="course-session-id"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
