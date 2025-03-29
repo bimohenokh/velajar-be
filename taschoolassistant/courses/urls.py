d80341554cfc2b16cf2d09be0aae55293f6c72b5
bimohenokh <bimo.barata2@gmail.com>
2025-03-29 11:27:21 +0700

    fixing settings.py - fix env variable now always using environ     - adding .env.example for environment example - fix media path and static path     - moving courses-banner to fixture but when you want to get using url move it to /media/courses-banner/ - adding all models into admin.py

--- taschoolassistant/courses/urls.py
+++ taschoolassistant/courses/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.urls import path
 from .views import CourseView, CourseViewById, CourseSessionView, CourseSessionViewById
 
@@ -8,5 +7,3 @@ urlpatterns = [
     path("course/<int:course_id>/", CourseSessionView.as_view(), name="course-session"),
     path("course/<int:course_id>/session/<int:session_id>/", CourseSessionViewById.as_view(), name="course-session-id"),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
