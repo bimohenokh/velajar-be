15ad1d431279344b9a73c09cd59f32b80dd82c1e
bimohenokh <bimo.barata2@gmail.com>
2025-03-29 11:27:21 +0700

    fixing settings.py - fix env variable now always using environ     - adding .env.example for environment example - fix media path and static path     - moving courses-banner to fixture but when you want to get using url move it to /media/courses-banner/ - adding all models into admin.py

--- taschoolassistant/courses/urls.py
+++ taschoolassistant/courses/urls.py
@@ -1,11 +1,7 @@
-from django.conf import settings
 from django.urls import path
 from .views import CourseView, CourseViewById
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("", CourseView.as_view(), name="course"),
     path("<int:pk>", CourseViewById.as_view(), name="course-id"),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- taschoolassistant/urls.py
+++ taschoolassistant/urls.py
@@ -14,21 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
+from taschoolassistant import settings
 
-from taschoolassistant.coba import views
 
-# router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    # path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path("api/users/", include("taschoolassistant.users.urls")),
     path("api/courses/", include("taschoolassistant.courses.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
