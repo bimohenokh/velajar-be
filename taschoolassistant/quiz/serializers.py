778e121398841a6c00ca760b6a5d55cffdfae126
bayurisma <maulanabayurisma@gmail.com>
2025-05-02 17:29:45 +0700

    add more functionality for quiz feature

--- taschoolassistant/quiz/serializers.py
+++ taschoolassistant/quiz/serializers.py
@@ -1,5 +1,5 @@
 # quizzes/serializers.py
-from datetime import timezone
+from django.utils import timezone
 from rest_framework import serializers
 from .models import Quiz, Question, Option, QuizAttempt, Answer
 
@@ -15,12 +15,35 @@ class QuestionSerializer(serializers.ModelSerializer):
         model = Question
         fields = ['id', 'text', 'type', 'options']
 
-class QuizSerializer(serializers.ModelSerializer):
-    questions = QuestionSerializer(many=True, read_only=True)
+    def validate(self, data):
+        if 'options' in data:
+            options = data.get('options', [])
+            if len(options) < 2:
+                raise serializers.ValidationError("A question must have at least two options.")
 
+            correct_options = [opt for opt in options if opt.get('is_correct')]
+            q_type = data.get('type')
+
+            if q_type == 'single' and len(correct_options) != 1:
+                raise serializers.ValidationError("Single choice questions must have exactly one correct option.")
+            elif q_type == 'multiple' and len(correct_options) < 2:
+                raise serializers.ValidationError("Multiple choice questions must have at least two correct options.")
+
+        return data
+
+    def create(self, validated_data):
+        options_data = validated_data.pop('options', [])
+        quiz = self.context['quiz']
+        validated_data.pop('quiz', None)  # buang jika sudah ada
+        question = Question.objects.create(quiz=quiz, **validated_data)
+        for opt in options_data:
+            Option.objects.create(question=question, **opt)
+        return question
+
+class QuizSerializer(serializers.ModelSerializer):
     class Meta:
         model = Quiz
-        fields = ['id', 'title', 'description', 'course_session' 'total_points', 'time_range', 'status']
+        fields = ['id', 'title', 'description', 'course_session', 'total_points', 'time_range', 'status']
 
 class AnswerSerializer(serializers.ModelSerializer):
     selected_options = serializers.PrimaryKeyRelatedField(
@@ -30,6 +53,26 @@ class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
         fields = ['question', 'selected_options']
+    
+    def validate(self, data):
+        question = data.get('question')
+        selected_options = data.get('selected_options')
+
+        if not question:
+            raise serializers.ValidationError("Question is required.")
+
+        if question.type == 'single':
+            if len(selected_options) != 1:
+                raise serializers.ValidationError("Single choice questions must have exactly one selected option.")
+
+        # Optional: pastikan selected options benar-benar milik pertanyaannya
+        options = Option.objects.filter(question=question)
+        valid_option_ids = set(options.values_list('id', flat=True))
+        for option in selected_options:
+            if option.id not in valid_option_ids:
+                raise serializers.ValidationError(f"Option {option.id} does not belong to the question.")
+
+        return data
 
 class QuizAttemptSerializer(serializers.ModelSerializer):
     answers = AnswerSerializer(many=True, write_only=True)
@@ -37,29 +80,34 @@ class QuizAttemptSerializer(serializers.ModelSerializer):
     class Meta:
         model = QuizAttempt
         fields = ['id', 'quiz', 'student', 'submitted_at', 'score', 'answers']
+        read_only_fields = ['score', 'submitted_at']
 
-    def create(self, validated_data):
-        answers_data = validated_data.pop('answers')
-        quiz_id = validated_data['quiz']
-        quiz = Quiz.objects.get(id=quiz_id)
-        attempt = QuizAttempt.objects.get(quiz=quiz)
+    def update(self, instance, validated_data):
+        answers_data = validated_data.pop('answers', [])
 
-        attempt.score = self.calculate_score(attempt, answers_data)
-        attempt.submitted_at = timezone.now()
-        attempt.is_submitted = True
-        attempt.save()
-        return attempt
+        instance.score = self.calculate_score(instance, answers_data)
+        instance.submitted_at = timezone.now()
+        instance.is_submitted = True
+        instance.save()
+        return instance
 
     def calculate_score(self, attempt, answers):
-        questions = Question.objects.get(quiz=attempt.quiz)
+        total_questions = attempt.quiz.questions.count()
         correct = 0
-        total = questions.count()
 
-        if total == 0:
+        if total_questions == 0:
             return 0
-        for answer in answers.all():
-            correct_options = set(answer.question.options.filter(is_correct=True).values_list('id', flat=True))
-            selected_options = set(answer.selected_options.all().values_list('id', flat=True))
-            if correct_options == selected_options:
+        for answer in answers:
+            question = answer['question']
+            if isinstance(question, Question):
+                question_id = question.id
+            else:
+                question_id = question
+            selected_option_ids = set(answer['selected_options'])
+
+            question = Question.objects.get(id=question_id)
+            correct_option_ids = set(question.options.filter(is_correct=True).values_list('id', flat=True))
+
+            if selected_option_ids == correct_option_ids:
                 correct += 1
-        return (correct / total) * attempt.quiz.total_points
+        return (correct / total_questions) * attempt.quiz.total_points
--- taschoolassistant/quiz/urls.py
+++ taschoolassistant/quiz/urls.py
@@ -1,6 +1,6 @@
 # quizzes/urls.py
 from django.urls import path
-from .views import QuizView, QuizDetailView, QuestionByQuizIdView, StartQuizView, StopQuizView, SaveAnswerView, StartAttemptView, SubmitQuizView
+from .views import QuizView, QuizDetailView, QuestionByQuizIdView, StartQuizView, StopQuizView, AnswerView, StartAttemptView, SubmitQuizView
 
 urlpatterns = [
     path('', QuizView.as_view(), name='quiz'),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('<int:quiz_id>/questions/', QuestionByQuizIdView.as_view(), name='quiz-questions'),
     path('<int:quiz_id>/start/', StartQuizView.as_view(), name='start-quiz'),
     path('<int:quiz_id>/stop/', StopQuizView.as_view(), name='stop-quiz'),
-    path('save-answer/', SaveAnswerView.as_view(), name='save-answer'),
-    path('start-attempt/', StartAttemptView.as_view(), name='start-attempt'),
+    path('answer/', AnswerView.as_view(), name='answer'),
+    path('<int:quiz_id>/start-attempt/', StartAttemptView.as_view(), name='start-attempt'),
     path('submit/', SubmitQuizView.as_view(), name='submit-quiz'),
 ]
