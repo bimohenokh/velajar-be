8b34c8bdd926e6bce6edfe5f168f47e6a4ccc779
Maulana Bayu Risma Santoso Sari <maulana.bayu@ui.ac.id>
2025-05-11 21:56:34 +0700

    MR Quiz feature to staging

--- taschoolassistant/courses/serializers.py
+++ taschoolassistant/courses/serializers.py
@@ -12,6 +12,27 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = "__all__"
 
+    @override
+    def update(self, instance, validated_data):
+        """Update instance and delete old image file if a new one is provided"""
+        # TODO misal kalau user upload gambar tapi nama file sama dengan yang udah ada
+        # TODO kalau bulk_update kgk bakal diexecute
+        # TODO handle transaction
+        old_image_path = instance.image_banner.path if instance.image_banner else None
+        new_image = validated_data.get("image_banner", None)
+
+        with transaction.atomic():  # Ensure database update is atomic
+            updated_instance = super().update(instance, validated_data)
+
+            # Only delete the old image if a new one is uploaded and is different
+            if new_image and old_image_path and old_image_path != updated_instance.image_banner.path:
+                if default_storage.exists(old_image_path):
+                    try:
+                        default_storage.delete(old_image_path)
+                    except Exception as e:
+                        print(f"Failed to delete old image: {e}")
+
+        return updated_instance
 
 
 class CourseParticipantSerializer(serializers.ModelSerializer):
