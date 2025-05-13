5619faa21dacd8df4f64037de262bdf04444da14
bimohenokh <bimo.barata2@gmail.com>
2025-05-13 18:35:41 +0700

    berhasil lah ini

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-#echo "Waiting for PostgreSQL to start..."
-## TODO nanti secret, admin, backend_db jadi env
-#until PGPASSWORD=secret psql -h db -U admin -d backend_db -c "SELECT 1" > /dev/null 2>&1; do
-#  sleep 1
-#done
-
-echo "Running collectstatic..."
-python manage.py collectstatic --noinput
-
 echo "Running migrations..."
 python manage.py migrate
 
