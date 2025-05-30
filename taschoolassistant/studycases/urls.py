82e0038cdba4cdab188e8a386036c18bbc867f32
bimohenokh <bimo.barata2@gmail.com>
2025-05-30 14:44:14 +0700

    fix url studycase

--- taschoolassistant/studycases/urls.py
+++ taschoolassistant/studycases/urls.py
@@ -12,8 +12,8 @@ from .views import (
 urlpatterns = [
     path("", StudyCaseView.as_view(), name="study-case"),
     path("<int:case_id>/", StudyCaseViewById.as_view(), name="study-case-id"),
-    path("<int:case_id>/start", StartStudyCaseView.as_view(), name="start-study-case"),
-    path("<int:case_id>/stop", StopStudyCaseView.as_view(), name="stop-study-case"),
+    path("<int:case_id>/start/", StartStudyCaseView.as_view(), name="start-study-case"),
+    path("<int:case_id>/stop/", StopStudyCaseView.as_view(), name="stop-study-case"),
     path("<int:case_id>/attempts/", StudyCaseAttemptView.as_view(), name="study-case-attempts"),
     path("attempts/<int:attempt_id>/", StudyCaseAttemptByIdView.as_view(), name="study-case-attempt-id"),
     path("attempts/<int:attempt_id>/answers/evaluate/", EvaluateStudyCaseAnswerView.as_view(), name="evaluate-study-case-attempt"),
