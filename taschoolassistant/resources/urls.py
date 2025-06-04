7b489b3f88672aae0cb4518222c408c9a918a5d2
bimohenokh <bimo.barata2@gmail.com>
2025-06-04 17:53:44 +0700

    lupa add / di resource url by id

--- taschoolassistant/resources/urls.py
+++ taschoolassistant/resources/urls.py
@@ -3,5 +3,5 @@ from .views import ResourceView, ResourceViewById
 
 urlpatterns = [
     path("", ResourceView.as_view(), name="resource"),
-    path("<int:resource_id>", ResourceViewById.as_view(), name="resource"),
+    path("<int:resource_id>/", ResourceViewById.as_view(), name="resource"),
 ]
